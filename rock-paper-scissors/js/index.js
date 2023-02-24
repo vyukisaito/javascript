1e04de33904a38e20b035496b4e03ac9cf22d703
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-02-23 22:34:24 -0300

    rock-paper-scissors

---
+++ rock-paper-scissors/js/index.js
@@ -0,0 +1,53 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('your-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+//com o querrySelectorAll, posso colocar uma variável para cada botão, no caso.
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 //ou você pode usar possibleChoices.length
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        resultDisplay.innerHTML = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        resultDisplay.innerHTML = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        resultDisplay.innerHTML = 'you lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        resultDisplay.innerHTML = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        resultDisplay.innerHTML = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        resultDisplay.innerHTML = 'you lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        resultDisplay.innerHTML = 'you win!'
+    }
+}
