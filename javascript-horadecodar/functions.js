b29bda77d0fea15b9e7988a395b4b95987ddaec3
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-03-09 22:55:17 -0300

    function exercise

---
+++ javascript-horadecodar/functions.js
@@ -0,0 +1,21 @@
+function primeiraFunção() {
+    console.log("Hello Wolrd")
+}
+primeiraFunção()
+// isso serve pra chamar a função, fazer isso para que ela funcione
+
+
+function dizerNome(nome) {
+    console.log('O nome é: ' + nome)
+}
+
+dizerNome('Name')
+dizerNome('Irineu')
+dizerNome('Dont')
+
+function soma(a, b) {
+    var soma = a + b
+    return soma
+}
+
+
