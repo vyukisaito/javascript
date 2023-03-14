62921ff04303c2205d6a27dac9415b43c5aae287
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-03-13 23:03:59 -0300

    let-const

---
+++ javascript-horadecodar/letconst.js
@@ -0,0 +1,22 @@
+let x = 5 
+x = 12
+
+const y = 34//constante
+
+
+console.log(x, y)
+
+if(true) {
+    let x = 20
+    const y= 36
+    /**Ao invés do var, esse fica no escopo local */
+    
+    console.log(x, y)
+}
+
+console.log(x)
+
+for (r = 1; r < 10; r++ ) {
+
+    console.log(r)
+}
