031a4049e94139c51d5dac7178020ad1a595bbf8
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-03-17 20:32:40 -0300

    update

---
+++ javascript-horadecodar/metodos-numericos.js
@@ -0,0 +1,20 @@
+//parseFloat transforma uma string em flutuante, com virgula
+
+console.log(parseFloat('34.4'))
+console.log(Number.parseFloat('34.4'))
+
+//parseInt
+console.log(parseInt('10'))
+console.log(parseInt('10.9'))
+
+//toFixed arredonda
+console.log(34.43435.toFixed(1))
+
+//isNaN
+console.log(isNaN('teste'))
+console.log(isNaN (23))
+console.log(isNaN('34'))
+
+//MAX_VALUE e MIN_VALUE maximo e minimo valor do js
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
