3e137533e95553354089b88c266554fa48b59cf6
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-04-27 18:22:24 -0300

    update

--- udemy/challenge01.js
+++
@@ -1,9 +0,0 @@
-var MarkW = 78
-var MarkT = 1.69
-var JohnW = 95
-var JohnT = 1.95
-var IMC = (MarkW / (MarkT * MarkT)).toFixed(1)
-var IMCJ = (JohnW / (JohnT * JohnT)).toFixed(1)
-
-const markHigherBMI = IMC > IMCJ
-console.log(markHigherBMI, IMC, IMCJ)
---
+++ udemy/challenges.js
@@ -0,0 +1,48 @@
+//Challenge 01
+var MarkW = 78
+var MarkT = 1.69
+var JohnW = 95
+var JohnT = 1.95
+var IMC = (MarkW / (MarkT * MarkT)).toFixed(1)
+var IMCJ = (JohnW / (JohnT * JohnT)).toFixed(1)
+
+const markHigherBMI = IMC > IMCJ
+console.log(markHigherBMI, IMC, IMCJ)
+//---------------------------------------------
+//Chalenge 02
+if (IMC > IMCJ) {
+    console.log(`Mark's BMI (${IMC}) is higher then John's (${IMCJ})!`)
+} else {
+    console.log(`John's (${IMCJ}) BMI is higher then Mark's (${IMC})!`)
+}
+//-----------------------------------------------
+//Challenge 03
+var dolphinsAverage = (96 + 108 + 89) / 3
+var koalasAverage = (88 + 91 + 110) / 3
+if (dolphinsAverage > koalasAverage) {
+    console.log(`Dolphins wins (${dolphinsAverage.toFixed(1)}) Koalas (${koalasAverage.toFixed(1)}) `)
+} else if (dolphinsAverage === koalasAverage) {
+    console.log(`We have a draw, whit Dolphins scoring: ${dolphinsAverage.toFixed(1)} and koalas: ${koalasAverage.toFixed(1)} `)
+} else {
+    console.log(`Koalas wins (${koalasAverage.toFixed(1)}) Dolphins (${dolphinsAverage.toFixed(1)}) `)
+}
+
+var dolphinsAverage2 = (90 + 100 + 101) / 3
+var koalasAverage2 = (100 + 95 + 100) / 3
+
+if (dolphinsAverage2 < 100) {
+    console.log(`Dolphins lose because > 100 (${dolphinsAverage2.toFixed(1)}) `)
+
+} if (koalasAverage2 < 100) {
+    console.log(`Koalas lose beacause > 100 (${koalasAverage2.toFixed(1)}) `)
+
+} else if (dolphinsAverage2 > koalasAverage2) {
+    console.log(`Dolphins wins (${dolphinsAverage2.toFixed(1)}) Koalas (${koalasAverage2.toFixed(1)}) `)
+
+} else if (dolphinsAverage2 === koalasAverage2) {
+    console.log(`We have a draw, whit Dolphins scoring: ${dolphinsAverage2.toFixed(1)} and koalas: ${koalasAverage2.toFixed(1)} `)
+
+} else {
+    console.log(`Koalas wins (${koalasAverage2.toFixed(1)}) Dolphins (${dolphinsAverage2.toFixed(1)}) `)
+}
+
