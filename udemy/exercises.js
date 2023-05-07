99d0e9d097fd08e64fbc367a3bb043a234ec2d96
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-07 11:35:21 -0300

    exercise

--- udemy/exercises.js
+++ udemy/exercises.js
@@ -1,3 +1,4 @@
+/** 
 function logger() {
     console.log('Mu name is Yuki')
 }
@@ -7,11 +8,29 @@ logger()
 
 function fruitProcessor(apples, oranges) {
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-    return juice /*with this return, we can return any vallue from the function. So the juice can be used any else in the code*/
+    return juice /*with this return, we can return any vallue from the function. So the juice can be used any else in the code/*
 }
 
 const appleJuice = fruitProcessor(5, 0)
 console.log(appleJuice)
 
 const appleOrangeJuice = fruitProcessor(2, 4)
-console.log(appleOrangeJuice)
+console.log(appleOrangeJuice) 
+*/
+
+
+//Function Declaration
+function calAge1(birthYear) {
+    return 2023 - birthYear;
+    /**this function is called, funcrion declaration */
+}
+const age1 = calAge1(2008)/*this is called argument o que tá dentro do perentese*/
+console.log(age1)
+
+//Function Expression
+const calcage2 = function (birthYear2) {
+    return 2023 - birthYear2
+    /**this one is anonymas function or functio expretion */
+}
+const age2 = calcage2(2008)
+console.log(age1, age2)
