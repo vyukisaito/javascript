e8089ef251ab2ebe8869e3b6e72ba28bd1f58cab
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-09 22:25:29 -0300

    update

--- udemy/exercises.js
+++ udemy/exercises.js
@@ -18,31 +18,47 @@ const appleOrangeJuice = fruitProcessor(2, 4)
 console.log(appleOrangeJuice) 
 */
 
-
+/*
 //Function Declaration
 function calAge1(birthYear) {
     return 2023 - birthYear;
-    /**this function is called, funcrion declaration */
+    /**this function is called, funcrion declaration 
 }
-const age1 = calAge1(2008)/*this is called argument o que tá dentro do perentese*/
+const age1 = calAge1(2008)/*this is called argument o que tá dentro do perentese
 console.log(age1)
 
 //Function Expression
 const calcage2 = function (birthYear2) {
     return 2023 - birthYear2
-    /**this one is anonymas function or functio expretion */
+    /**this one is anonymas function or functio expretion 
 }
 const age2 = calcage2(2008)
 console.log(age1, age2)
 
 //Arrow function (short way to write a function expression)
-const calcAge3 = birthYear => 2023 - birthYear; /*com uma linha da função, não é necessário usar o 'return' */
+const calcAge3 = birthYear => 2023 - birthYear; /*com uma linha da função, não é necessário usar o 'return' 
 const age3 = calcAge3(2008);
 console.log(age3);
 
-const yearsRetirement = birthYear => {
+const yearsRetirement = (birthYear, firstName) => {
     const age = 2023 - birthYear;
     const retirement = 65 - age;
-    return retirement;
-} /*quando tem mais de uma linha na função, é necessário utilizar o 'return' */
-console.log(yearsRetirement(2008));
+    //return retirement; (se tivesse só um parametro, seria isso)
+    return `${firstName} retires in ${retirement} years`;
+} /*quando tem mais de uma linha na função, é necessário utilizar o 'return' 
+console.log(yearsRetirement(2008, 'Yuki'));
+console.log(yearsRetirement(1992, 'João'))
+*/
+
+function cutFruit(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruit(apples);
+    const orangePieces = cutFruit(oranges);
+
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3))
