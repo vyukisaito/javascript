d1095502ee6e8405987e0ba8e97edfedcd74d9d3
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-10 22:58:11 -0300

    exercises

--- udemy/challenges.js
+++ udemy/challenges.js
@@ -1,5 +1,5 @@
 //Challenge 01
-/*
+
 var MarkW = 78
 var MarkT = 1.69
 var JohnW = 95
@@ -9,22 +9,26 @@ var IMCJ = (JohnW / (JohnT * JohnT)).toFixed(1)
 
 const markHigherBMI = IMC > IMCJ
 console.log(markHigherBMI, IMC, IMCJ)
-*/
 
-//---------------------------------------------
+
+/////////////////////////////////////////////////////////
+console.log('-----------------------------------------')
+///////////////////////////////////////////////////////
 
 //Chalenge 02
-/*
+
 if (IMC > IMCJ) {
     console.log(`Mark's BMI (${IMC}) is higher then John's (${IMCJ})!`)
 } else {
     console.log(`John's (${IMCJ}) BMI is higher then Mark's (${IMC})!`)
-}*/
+}
 
-//-----------------------------------------------
+/////////////////////////////////////////////////////////
+console.log('-----------------------------------------')
+///////////////////////////////////////////////////////
 
 //Challenge 03
-/*var dolphinsAverage = (96 + 108 + 89) / 3
+var dolphinsAverage = (96 + 108 + 89) / 3
 var koalasAverage = (88 + 91 + 110) / 3
 if (dolphinsAverage > koalasAverage) {
     console.log(`Dolphins wins (${dolphinsAverage.toFixed(1)}) Koalas (${koalasAverage.toFixed(1)}) `)
@@ -48,11 +52,13 @@ if (dolphinsAverage2 > koalasAverage2 && koalasAverage2 >= 100) {
 } else {
     console.log("The teams din't did 100 or more points")
 }
-*/
-//-----------------------------------------------
+
+/////////////////////////////////////////////////////////
+console.log('-----------------------------------------')
+///////////////////////////////////////////////////////
 
 //Challenge 04
-/*
+
 var bill = 430
 
 console.log(`The bill is: ${bill} the tip is: ${bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2} the total is: ${bill >= 50 && bill <= 300 ? bill * 0.15 + bill : bill * 0.2 + bill}`)
@@ -61,5 +67,20 @@ console.log(`The bill is: ${bill} the tip is: ${bill >= 50 && bill <= 300 ? bill
 const bill2 = 275;
 const tip2 = bill2 <= 300 && bill2 >= 50 ? bill2 * 0.15 : bill2 * 0.2;
 console.log(`The bill was ${bill2}, the tip was ${tip2}, and the total value is ${bill2 + tip2}`)
-*/
-//-------------------------------------------------
+
+/////////////////////////////////////////////////////////
+console.log('-----------------------------------------')
+///////////////////////////////////////////////////////
+
+//Challenge 05
+
+const calcAverage = (dolphin, koala) => {
+    return `Dolphins scored ${dolphin} points and Koalas ${koala} points`;
+}
+console.log(calcAverage((44 + 23 + 71) / 3, (65 + 54 + 49) / 3))
+
+function chekWinner(dolphinAveragee, koalasAveragee) {
+    if (dolphinAveragee >= koalasAveragee * 2) {
+
+    }
+}
