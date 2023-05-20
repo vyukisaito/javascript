d45f431bbd997ca9b9902386594bfa158edd72a1
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-20 17:34:32 -0300

    update

--- udemy/exercises.js
+++ udemy/exercises.js
@@ -1,4 +1,4 @@
-
+/*
 function logger() {
     console.log('Mu name is Yuki')
 }
@@ -12,7 +12,7 @@ console.log('-----------------------------------------')
 
 function fruitProcessor(apples, oranges) {
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-    return juice /*with this return, we can return any vallue from the function. So the juice can be used any else in the code*/
+    return juice //with this return, we can return any vallue from the function. So the juice can be used any else in the code
 }
 
 const appleJuice = fruitProcessor(5, 0)
@@ -28,15 +28,15 @@ console.log('-----------------------------------------')
 //Function Declaration
 function calAge1(birthYear) {
     return 2023 - birthYear;
-    /**this function is called, funcrion declaration */
+    //this function is called, funcrion declaration 
 }
-const age1 = calAge1(2008)/*this is called argument o que tá dentro do perentese
-console.log(age1)*/
+const age1 = calAge1(2008)///*this is called argument o que tá dentro do perentese
+console.log(age1)
 
 //Function Expression
 const calcage2 = function (birthYear2) {
     return 2023 - birthYear2
-    /**this one is anonymas function or functio expretion */
+   // /this one is anonymas function or functio expretion 
 }
 const age2 = calcage2(2008)
 console.log(age1, age2)
@@ -46,7 +46,7 @@ console.log('-----------------------------------------')
 ////////////////////////////////////////////////////////
 
 //Arrow function (short way to write a function expression)
-const calcAge3 = birthYear => 2023 - birthYear; /*com uma linha da função, não é necessário usar o 'return' */
+const calcAge3 = birthYear => 2023 - birthYear; ///*com uma linha da função, não é necessário usar o 'return' 
 const age3 = calcAge3(2008);
 console.log(age3);
 
@@ -55,7 +55,7 @@ const yearsRetirement = (birthYear, firstName) => {
     const retirement = 65 - age;
     //return retirement; (se tivesse só um parametro, seria isso)
     return `${firstName} retires in ${retirement} years`;
-} /*quando tem mais de uma linha na função, é necessário utilizar o 'return' */
+} ///*quando tem mais de uma linha na função, é necessário utilizar o 'return' 
 console.log(yearsRetirement(2008, 'Yuki'));
 console.log(yearsRetirement(1992, 'João'))
 
@@ -164,4 +164,5 @@ const jonasObject = {
     age: 2037 - 1991,
     friendsss: ['Michael', 'Peter', 'Steven']
 }
-console.log(jonasObject, jonasArray)
+console.log(jonasObject, jonasArray) 
+*/
