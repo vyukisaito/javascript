d54f0d3262019b563f7a6a86d85b594f1f79bb69
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-26 16:58:13 -0300

    update

--- exercicios/exercicios/analisador02/js/script.js
+++ exercicios/exercicios/analisador02/js/script.js
@@ -1,25 +1,65 @@
 let numberInput = document.getElementById('num')
 let numberSelect = document.getElementById('selNum')
+let res = document.getElementById('res')
 let num = []
 
-function checar(num) {
-    if (Number(num) >= 1 && Number(num) <= 100) {
+function checar(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
     }
 }
 
-
-
-function adicionar() {
-    num.push(numberInput.value)
-    console.log(num)
-    if (Number(num) >= 1 && Number(num) <= 100 || Number(num) <= 0) {
-        alert('Valor não pode')
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        //indexOf serve para encontrar um valor especifico na array
+        return true
     } else {
-        alert("Funcionando")
+        return false
     }
-
 }
 
+function adicionar() {
+    if (checar(numberInput.value) && !inList(numberInput.value, num)) {
+        num.push(Number(numberInput.value))
+        let item = document.createElement('option')
+        item.text = `O valor ${numberInput.value} foi adicionado`
+        numberSelect.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Ou o valor é inválido, ou já foi preenchido')
+    }
+
+    numberInput.value = ''
+    numberInput.focus()
+}
+
+function analisar() {
+    if (num.length == 0) {
+        window.alert('Adiocione valores antes de finalizar')
+    } else {
+        let total = num.length
+        let maior = num[0]
+        let menor = num[0]
+        let soma = 0
+        let media = 0
+
+        for (let pos in num) {
+            soma += num[pos]
+            if (num[pos] > maior)
+                maior = num[pos]
+
+            if (num[pos] < menor)
+                menor = num[pos]
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menos valor infomado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma dos valores inserido é de ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores inserido é de ${media.toFixed(1)} </p>`
+        console.log(num[0])
+    }
+}
