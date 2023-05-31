0179324af5c7c59a23d52cd5264da978f311739b
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-30 23:06:47 -0300

    test

--- dice-counter/script.js
+++ dice-counter/script.js
@@ -1,6 +1,13 @@
 let dice = Math.trunc(Math.random() * 6) + 1;
-let num = document.getElementById('num')
+let imagem = "POADML"
+let imagemAção = Math.random(imagem)
+let num = document.getElementById('resNum')
+let resIm = document.getElementById('resIm')
 
 function dado() {
     num.innerHTML = dice
+}
+
+function imagem() {
+    resIm.innerHTML = imagemAção
 }
