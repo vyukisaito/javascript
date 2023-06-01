dbd685f686efc59462c6eb4c6bc92053eafc1664
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-31 22:25:13 -0300

    update

--- dice-counter/script.js
+++ dice-counter/script.js
@@ -1,20 +1,131 @@
-let dice = Math.trunc(Math.random() * 6) + 1;
+'use strict'
+
 let num = document.getElementById('resNum')
 
-let imagem = "POADML"
-let imagemAção = Math.random(imagem)
-let letrasArray = letras.split('');
+//let imagemAção = Math.random(imagem)
 let resIm = document.getElementById('resIm')
 
 function dado() {
+    let dice = Math.trunc(Math.random() * 6) + 1;
     num.innerHTML = dice
 }
 
+
+let letras = 'POADML';
+
 function imagemF() {
-    return Math.random() - 0.5
+    let indiceSorteado = Math.floor(Math.random() * letras.length);
+    let letraSorteada = letras[indiceSorteado];
+
+    return resIm.innerHTML = `${letraSorteada}`
 }
 
-letrasArray.sort(compararAleatorio);
-let letrasEmbaralhadas = letrasArray.join('');
+/*
+let cronometro = document.getElementById('cro')
+let mil = 00
+let sec = 00
+let min = 00
+let hra = 00
+let isRunning = false
+let intervalo
 
-console.log(letrasEmbaralhadas);
+function digits(digit) {
+    if(digits < 10) {
+        return ('0' = digit)
+    } else {
+        return (digit)
+    }
+}
+
+function start() {
+    if (isRunning === false) {
+        watch();
+        intervalo = setInterval((watch), 10)
+    }
+    isRunning = true
+}
+
+function pause() {
+    clearInterval(intervalo)
+    isRunning = false
+}
+
+function restart() {
+    clearInterval(intervalo)
+    mil = 0
+    sec = 0
+    min = 0
+    hra = 0
+    cronometro.innerHTML = '00:00:00;00'
+    isRunning = false
+}
+
+function watch() {
+    mil++
+    if (mil == 100) {
+        sec++
+        mil = 0
+    } else if (sec == 60) {
+        min++
+        sec = 0
+    } else if (min = 60) {
+        hra++
+        min = 0
+    }
+
+    cronometro.innerHTML = digits(hra) + ':' + digits(min) + ':' + digits(sec) + ';' + digits(mil)
+}
+*/
+
+let mil = 00
+let sec = 00
+let min = 00
+let hra = 00
+let isRunning = false
+let intervalo
+
+function digits(digit) {
+    if (digit<10) {
+        return('0'+ digit )
+    } else {
+        return (digit)
+    }
+}
+
+function start() {
+    if (isRunning === false) {
+        watch();
+        intervalo = setInterval((watch), 10);
+    }
+    isRunning = true
+}
+
+function pause() {
+    clearInterval(intervalo)
+    isRunning = false
+}
+
+function restart() {
+    clearInterval(intervalo)
+    mil = 0
+    sec = 0
+    min = 0
+    hra = 0
+    document.getElementById('cro').innerHTML = '00:00:00,00'
+    isRunning = false
+}
+
+function watch() {
+    mil++
+    if (mil == 100) {
+        sec++
+        mil = 0
+    } else if (sec == 60) {
+        min++
+        sec = 0
+    } else if (min == 60) {
+        hra++
+        min = 0
+    }
+    document.getElementById('cro').innerHTML = digits(hra) + ':' + digits(min) + ':' + digits(sec) + ',' + digits(mil)
+}
