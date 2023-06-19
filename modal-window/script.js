e2a97f645cebcfb0b8a9c95e8d990bc1b78bc672
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-06-18 22:57:33 -0300

    update

---
+++ modal-window/script.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const hidden = document.querySelector('.hidden')
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener ('click', openModal);
+
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+btnClose.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+    }
+})
