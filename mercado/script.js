a4f55e709b1d74489d6b7b0f629f400ac5cf0830
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-10-23 21:43:11 -0300

    update

--- mercado/script.js
+++ mercado/script.js
@@ -3,5 +3,24 @@ let compra = document.getElementById('compras')
 let compras = []
 
 function add() {
-    compra.innerHTML = texto.value
+    let item = document.createElement("option")
+    item.text = `${texto.value}`
+    compra.appendChild(item)
+    texto.value = ""
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-----Starting exercise ${exercise}`)
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+    }
+}
+exercise = 1
+
+//While loop
+let rep = 1;
+while (rep <= 10) {
+    console.log(`${rep}`);
+    rep++;
 }
