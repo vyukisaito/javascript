6346693d9d3f1c0d4654c7f964e62dd568d60595
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-10-27 21:50:52 -0300

    update

---
+++ udemy/ex3.js
@@ -0,0 +1,13 @@
+console.log(this)
+
+const calcAge = function(birthYear) {
+    console.log(2037 - birthYear)
+    console.log(this)
+}
+calcAge(1991)
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear)
+    console.log(this)
+}
+calcAge(1980)
